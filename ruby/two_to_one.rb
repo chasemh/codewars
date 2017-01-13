83bf8678613ca7896a11400f22056a427690d9d3
Chase Maxwell Hennion <chase.m.hennion@seagate.com>
2017-01-12 21:44:21 -0600

    Ading two to one solution.

---
+++ ruby/two_to_one.rb
@@ -0,0 +1,20 @@
+# Two to One Solution
+#
+# Author: Chase Hennion <chase_hennion@outlook.com>
+
+# Problem Statement
+#
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, - each taken only once - coming from s1 or s2.
+#
+# Examples:
+#
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+#
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+def longest(a1, a2)
+  return (a1 << a2).split('').sort!.uniq!.join('')
+end
